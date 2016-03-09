1040084167e201ca252c7abdfe1a76a52d16ffde
Jon Wrona <jonwrona@gmail.com>
2016-03-08 23:05:21 -0500

    removed node_modules from remote

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
-app.get('*', function(req, res) {
-	res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+app.get('/', function(req, res) {
+	res.send('Welcome to Table Tome.')
 });
 
 app.listen(app.get('port'), function() {
