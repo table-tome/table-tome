96acd2520fe0e36adb8ccb700786f0d2a76853f2
Jon Wrona <jonwrona@gmail.com>
2016-03-26 19:10:46 -0400

    removed a bunch of stuff to start grunt testing from scratch

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ app.set('port', (process.env.PORT || 5000));
 // app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-	res.send('Welcome to Table Tome.')
+  res.send('Welcome to Table Tome.')
 });
 
 app.listen(app.get('port'), function() {
-	console.log('TableTome is running on port', app.get('port'));
+  console.log('TableTome is running on port', app.get('port'));
 });
