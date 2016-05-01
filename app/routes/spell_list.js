7ad0f9f059b4991016fa9022994c46698f906b4c
Jon Wrona <jonwrona@gmail.com>
2016-05-01 15:31:40 -0400

    added route for creating a new spell list

--- app/routes/spell_list.js
+++ app/routes/spell_list.js
@@ -4,13 +4,53 @@ module.exports = function(app, express, authenticate, auth0Manager) {
   listRouter.use('/', authenticate);
 
   listRouter.get('/', function(req, res) {
+    var user_id = req.user.sub;
     // get the current user's information using their ID provided 
     //  by the authentication middleware
-    auth0Manager.getUser({ id: req.user.sub }, function(err, user) {
+    auth0Manager.getUser({ id: user_id }, function(err, user) {
       if (err) res.send(err);
       else {
         // return only the user's spell lists
         res.send(user.user_metadata.spell_lists || {});
+      }
+    });
+  });
+
+  listRouter.post('/', function(req, res) {
+    var user_id = req.user.sub;
+    // make sure the new list name isn't undefined
+    if (!req.body.list_name) {
+      res.send("A name for the new spell list must be provided");
+      return;
+    }
+    // get the current user in order to update the metadata without
+    //  overwriting their lists
+    auth0Manager.getUser({ id: user_id }, function(err, user) {
+      if (err) res.send(err);
+      else {
+        var lists = [];
+        if (user.user_metadata && user.user_metadata.spell_lists)
+          lists = user.user_metadata.spell_lists;
+
+        // make sure that no other list has the same name
+        for (i in lists) {
+          if (lists[i].name === req.body.list_name) {
+            res.send("A spell list named \"" + req.body.list_name + "\" already exists");
+            return;
+          }
+        }
+
+        // create a new empty list, and add it to the user's metadata in auth0
+        var new_list = { name: req.body.list_name, list: [] };
+        lists.push(new_list);
+        var metadata = { spell_lists: lists };
+        auth0Manager.updateUserMetadata({ id: user_id }, metadata, function(err, user) {
+          if (err) res.send(err);
+          else {
+            // return the user's updated spell lists
+            res.send(user.user_metadata.spell_lists || []);
+          }
+        });
       }
     });
   });
