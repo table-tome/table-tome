ebcd0818a0b3e2ff93f708ca7c53d01c414f367e
Daniel Lowe <danlowe95@gmail.com>
2016-05-06 15:41:02 -0400

    wip

--- public/app/controllers/profile.controller.js
+++ public/app/controllers/profile.controller.js
@@ -2,17 +2,10 @@ angular.module('profile.controller', [])
   .controller('profileCtrl', ['$scope', '$routeParams', 'auth', 'User', function($scope, $routeParams, auth, User) {
 
     $scope.userUrl = $routeParams.username;
-    $scope.user = null;
-
-    User.get($scope.userUrl).success(function(data) {
-      console.log(data);
-      $scope.user = data;
-    });
-
-    $scope.user = {
-      nickname: 'Fake',
+    $scope.fakeUser = {
+      nickname: 'No User',
       picture: 'http://www.tribe-war.com/images/user.jpg',
-      user_id: 'fakeid123456789'
+      user_id: 'fakeid123456789',
       user_metadata: {
         spell_lists: [{
           name: 'List 1',
@@ -29,6 +22,21 @@ angular.module('profile.controller', [])
         about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam commodo metus nec convallis facilisis. Aliquam sit amet convallis lacus. Pellentesque sit amet lacinia leo, sit amet accumsan ex. Sed eleifend ultricies tellus. Proin sit amet ullamcorper nunc. Proin mauris massa, vulputate sed orci vitae, pharetra lacinia sem. Etiam quis ipsum dolor. Suspendisse commodo, purus sit amet rutrum mollis, metus purus fermentum mi, ac consequat ligula ex nec nisl.'
       }
     };
+    // $scope.parseData = function() {
+    //   if ($scope.user == null){
+    //     $scope.user = $scope.fakeUser;
+    //   }
+    // };
+    User.get($scope.userUrl).success(function(data) {
+      console.log(data);
+      $scope.user = data;
+      if ($scope.user == []) {
+        console.log("it was null");
+        $scope.user = $scope.fakeUser;
+      }
+    });
+
+
 
     $scope.auth = auth;
   }]);
